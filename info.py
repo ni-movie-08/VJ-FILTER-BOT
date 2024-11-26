1d47ba1149b3d09bc3e8a801684a8a5a5d2349d0
──「 ʟᴜᴄɪғᴇʀ 」── <nimovie08@gmail.com>
2024-11-26 16:57:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -66,7 +66,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 # MongoDB information
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nk27548990:yzVj9OJBZgZgBU0m@cluster0.wdei2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://lucifer00007860:wsK7LVPHAPqnyAs7@cluster0.lpsb0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjclonefilterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
